428a0f0dba43b872690a5a30d88189faa15298df
Eric Clark <eclark0013@gmail.com>
2019-06-17 15:41:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
 end
-
+programmer_hash[:dennis_ritchie][:languages]
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
@@ -80,7 +80,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto]=>
+  {
+		:known_for => "Ruby",
+    :languages => ["LISP", "C"]
+	}
 
 end
 
